7c40279be8cd7fbf43cf9739f7149a2388e36e3e
andreigarbo <andrei.garbo@gmail.com>
2026-05-03 10:02:34 +0300

    Scaled carousel

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,108 +1,121 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-const ChevronLeftPath = "/src/assets/svg/chevron-left-svgrepo-com.svg";
-const ChevronRightPath = "/src/assets/svg/chevron-right-svgrepo-com.svg";
+const ChevronLeftPath = '/src/assets/svg/chevron-left-svgrepo-com.svg';
+const ChevronRightPath = '/src/assets/svg/chevron-right-svgrepo-com.svg';
 const props = defineProps<{
-    imagePaths: string[],
-    width?: number,
-    height?: number
+  imagePaths: string[];
+  width?: number;
+  height?: number;
 }>();
 
-type switchImageDirection = "prev" | "next" | number;
+type switchImageDirection = 'prev' | 'next' | number;
 
 let currentImageId = 0;
 let currentImagePath = ref('');
 currentImagePath.value = props.imagePaths[0];
 
 function switchImage(directionOrId: switchImageDirection) {
-    if (directionOrId == "prev") {
-        if (currentImageId == 0) {
-            currentImageId = props.imagePaths.length - 1;
-        } else {
-            currentImageId -= 1;
-        }
-    } else if (directionOrId == "next") {
-        if (currentImageId == props.imagePaths.length - 1) {
-            currentImageId = 0;
-        } else {
-            currentImageId += 1;
-        }
+  if (directionOrId == 'prev') {
+    if (currentImageId == 0) {
+      currentImageId = props.imagePaths.length - 1;
     } else {
-        currentImageId = directionOrId;
+      currentImageId -= 1;
     }
-    currentImagePath.value = props.imagePaths[currentImageId];
+  } else if (directionOrId == 'next') {
+    if (currentImageId == props.imagePaths.length - 1) {
+      currentImageId = 0;
+    } else {
+      currentImageId += 1;
+    }
+  } else {
+    currentImageId = directionOrId;
+  }
+  currentImagePath.value = props.imagePaths[currentImageId];
 }
 
 function switchImagePrevious() {
-    switchImage("prev");
+  switchImage('prev');
 }
 
 function switchImageNext() {
-    switchImage("next");
+  switchImage('next');
 }
 </script>
 
 <template>
-    <div id="container">
-        <img :src="currentImagePath">
-        <button class="chevron-button" id="prev-img-button" @click="switchImagePrevious">
-            <img class="chevron" :src="ChevronLeftPath">
-        </button>
-        <button class="chevron-button" id="next-img-button" @click="switchImageNext">
-            <img class="chevron" :src="ChevronRightPath">
-        </button>
+  <div id="carousel-container">
+    <div id="image-container">
+      <img class="carousel-image" :src="currentImagePath" />
+      <button class="chevron-button" id="prev-img-button" @click="switchImagePrevious">
+        <img class="chevron" :src="ChevronLeftPath" />
+      </button>
+      <button class="chevron-button" id="next-img-button" @click="switchImageNext">
+        <img class="chevron" :src="ChevronRightPath" />
+      </button>
     </div>
     <div id="preview-selector">
-        <img v-for="(image, index) in props.imagePaths" :src="image" :key="index" alt="test" @click="switchImage(index)"/>
+      <img
+        v-for="(image, index) in props.imagePaths"
+        :src="image"
+        :key="index"
+        alt="test"
+        @click="switchImage(index)"
+      />
     </div>
+  </div>
 </template>
 
 <style>
+.carousel-image {
+  height: 80vh;
+  width: auto;
+  vertical-align: middle;
+}
 
 .chevron-button {
-    border: transparent;
-    background-color: transparent;
-    transition: 0.2s;
-    opacity: 100%;
+  border: transparent;
+  background-color: transparent;
+  transition: 0.2s;
+  opacity: 100%;
 }
 
-.chevron-button:hover{
-    opacity: 30%;
+.chevron-button:hover {
+  opacity: 30%;
 }
 
-#container {
-    position: relative;
+#image-container {
+  position: relative;
 }
 
 #prev-img-button {
-    position: absolute;
-    top: 50%;
-    left: 30px;
+  position: absolute;
+  top: 50%;
+  left: 7%;
 }
 
 #next-img-button {
-    position: absolute;
-    top: 50%;
-    right: 30px;
+  position: absolute;
+  top: 50%;
+  right: 7%;
 }
 
 .chevron {
-    height: 35px;
-    width: auto;
+  height: 35px;
+  width: auto;
 }
 
 #preview-selector {
-    padding-top: 5px;
-    display: flex;
-    justify-content: start;
-    overflow: scroll;
+  padding-top: 5px;
+  display: flex;
+  justify-content: start;
+  overflow: scroll;
 }
 
 #preview-selector img {
-    height: 90px;
-    width: auto;
-    margin: 0;
-    margin-right: 5px;
-    padding: 0;
+  height: 90px;
+  width: auto;
+  margin: 0;
+  margin-right: 5px;
+  padding: 0;
 }
 </style>
